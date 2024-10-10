d21222ae2e81595da1b3db413410100e97343393
Sean Oxley <sean.oxley@bibo.com.ph>
2024-10-10 17:07:51 +0900

    rustfmt

--- src/sys/lib.rs
+++ src/sys/lib.rs
@@ -1,16 +1,15 @@
-
 pub mod mux {
-    use std::os::raw::{c_void, c_char, c_int};
+    use std::os::raw::{c_char, c_int, c_void};
     use std::ptr::NonNull;
 
     #[repr(C)]
-    pub struct IWriter { _opaque_c_aligned: *mut c_void }
+    pub struct IWriter {
+        _opaque_c_aligned: *mut c_void,
+    }
     pub type WriterMutPtr = *mut IWriter;
     pub type WriterNonNullPtr = NonNull<IWriter>;
 
-    pub type WriterWriteFn = extern "C" fn(*mut c_void,
-                                           *const c_void,
-                                           usize) -> bool;
+    pub type WriterWriteFn = extern "C" fn(*mut c_void, *const c_void, usize) -> bool;
     pub type WriterGetPosFn = extern "C" fn(*mut c_void) -> u64;
     pub type WriterSetPosFn = extern "C" fn(*mut c_void, u64) -> bool;
     pub type WriterElementStartNotifyFn = extern "C" fn(*mut c_void, u64, i64);
@@ -25,12 +24,16 @@ pub mod mux {
     pub const AV1_CODEC_ID: u32 = 2;
 
     #[repr(C)]
-    pub struct Segment { _opaque_c_aligned: *mut c_void }
+    pub struct Segment {
+        _opaque_c_aligned: *mut c_void,
+    }
     pub type SegmentMutPtr = *mut Segment;
     pub type SegmentNonNullPtr = NonNull<Segment>;
 
     #[repr(C)]
-    pub struct Track { _opaque_c_aligned: *mut c_void }
+    pub struct Track {
+        _opaque_c_aligned: *mut c_void,
+    }
     pub type TrackMutPtr = *mut Track;
     pub type TrackNonNullPtr = NonNull<Track>;
 
@@ -44,15 +47,16 @@ pub mod mux {
     pub type AudioTrackMutPtr = *mut AudioTrack;
     pub type AudioTrackNonNullPtr = NonNull<AudioTrack>;
 
-
     #[link(name = "webmadapter", kind = "static")]
     extern "C" {
         #[link_name = "mux_new_writer"]
-        pub fn new_writer(write: Option<WriterWriteFn>,
-                          get_pos: Option<WriterGetPosFn>,
-                          set_pos: Option<WriterSetPosFn>,
-                          element_start_notify: Option<WriterElementStartNotifyFn>,
-                          user_data: *mut c_void) -> WriterMutPtr;
+        pub fn new_writer(
+            write: Option<WriterWriteFn>,
+            get_pos: Option<WriterGetPosFn>,
+            set_pos: Option<WriterSetPosFn>,
+            element_start_notify: Option<WriterElementStartNotifyFn>,
+            user_data: *mut c_void,
+        ) -> WriterMutPtr;
         #[link_name = "mux_delete_writer"]
         pub fn delete_writer(writer: WriterMutPtr);
 
@@ -60,7 +64,13 @@ pub mod mux {
         pub fn new_segment() -> SegmentMutPtr;
         #[link_name = "mux_initialize_segment"]
         pub fn initialize_segment(segment: SegmentMutPtr, writer: WriterMutPtr) -> bool;
-        pub fn mux_set_color(segment: VideoTrackMutPtr, bits: c_int, sampling_horiz: c_int, sampling_vert: c_int, full_range: c_int) -> c_int;
+        pub fn mux_set_color(
+            segment: VideoTrackMutPtr,
+            bits: c_int,
+            sampling_horiz: c_int,
+            sampling_vert: c_int,
+            full_range: c_int,
+        ) -> c_int;
         pub fn mux_set_writing_app(segment: SegmentMutPtr, name: *const c_char);
         #[link_name = "mux_finalize_segment"]
         pub fn finalize_segment(segment: SegmentMutPtr, duration: u64) -> bool;
@@ -73,21 +83,38 @@ pub mod mux {
         pub fn audio_track_base_mut(track: AudioTrackMutPtr) -> TrackMutPtr;
 
         #[link_name = "mux_segment_add_video_track"]
-        pub fn segment_add_video_track(segment: SegmentMutPtr,
-                                       width: i32, height: i32,
-                                       number: i32, codec_id: u32, id_out: *mut u64) -> VideoTrackMutPtr;
+        pub fn segment_add_video_track(
+            segment: SegmentMutPtr,
+            width: i32,
+            height: i32,
+            number: i32,
+            codec_id: u32,
+            id_out: *mut u64,
+        ) -> VideoTrackMutPtr;
         #[link_name = "mux_segment_add_audio_track"]
-        pub fn segment_add_audio_track(segment: SegmentMutPtr,
-                                       sample_rate: i32, channels: i32,
-                                       number: i32, codec_id: u32) -> AudioTrackMutPtr;
+        pub fn segment_add_audio_track(
+            segment: SegmentMutPtr,
+            sample_rate: i32,
+            channels: i32,
+            number: i32,
+            codec_id: u32,
+        ) -> AudioTrackMutPtr;
         #[link_name = "mux_segment_add_frame"]
-        pub fn segment_add_frame(segment: SegmentMutPtr,
-                                 track: TrackMutPtr,
-                                 frame: *const u8, length: usize,
-                                 timestamp_ns: u64, keyframe: bool) -> bool;
+        pub fn segment_add_frame(
+            segment: SegmentMutPtr,
+            track: TrackMutPtr,
+            frame: *const u8,
+            length: usize,
+            timestamp_ns: u64,
+            keyframe: bool,
+        ) -> bool;
         #[link_name = "mux_segment_set_codec_private"]
-        pub fn segment_set_codec_private(segment: SegmentMutPtr,
-                                         number: u64, data: *const u8, len: i32) -> bool;
+        pub fn segment_set_codec_private(
+            segment: SegmentMutPtr,
+            number: u64,
+            data: *const u8,
+            len: i32,
+        ) -> bool;
     }
 }
 
